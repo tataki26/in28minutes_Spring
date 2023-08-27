fbf8ea0e815788e2157a010b4c93fa26f62c3bfe
tataki26 <tataki26@gmail.com>
2023-08-27 20:53:41 +0900

    Java Gaming Application 시작하기 1
    - GamingRunner class
    - Game classes: Mario, SuperContra, Pacman, ...
    
    - GamingRunner 클래스가 MarioGame 클래스에 의존하고 있는 상태

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -0,0 +1,17 @@
+package com.in28minutes.learnspringframework.game;
+
+public class GameRunner {
+    MarioGame marioGame;
+    public GameRunner(MarioGame marioGame) {
+        this.marioGame = marioGame;
+    }
+
+    public void run() {
+        System.out.println("Running game: " + marioGame);
+
+        marioGame.up();
+        marioGame.down();
+        marioGame.left();
+        marioGame.right();
+    }
+}
