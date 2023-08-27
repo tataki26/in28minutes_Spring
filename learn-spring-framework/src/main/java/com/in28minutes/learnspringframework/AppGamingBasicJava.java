c0e8675d6188d812289a202f5d586625693f70a8
tataki26 <tataki26@gmail.com>
2023-08-27 21:13:48 +0900

    Java 인터페이스를 통해 느슨하게 결합된 앱 만들기 2
    * 연습 문제
    - pacman game을 추가하여 느슨한 결합 적용하기

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -2,17 +2,22 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
         MarioGame marioGame = new MarioGame();
         SuperContraGame superContraGame = new SuperContraGame();
+        PacmanGame pacmanGame = new PacmanGame();
 
         GameRunner marioGameRunner = new GameRunner(marioGame);
         marioGameRunner.run();
 
         GameRunner superContraGameRunner = new GameRunner(superContraGame);
         superContraGameRunner.run();
+
+        GameRunner pacmanGameRunner = new GameRunner(pacmanGame);
+        pacmanGameRunner.run();
     }
 }
