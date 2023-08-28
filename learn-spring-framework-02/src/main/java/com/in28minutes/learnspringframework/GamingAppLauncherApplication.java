36eacc0cd6e745a38e828fc05076e6f2deaf1ffe
tataki26 <tataki26@gmail.com>
2023-08-28 17:50:30 +0900

    Java 객체 생성 및 관리를 위한 Spring 프레임워크 이해하기 4
    - @Bean: 객체 직접 생성
    - @Component: Spring이 객체 생성
    
    - Spring: 객체 관리, auto-wiring, 객체 생성(@Component/@ComponentScan)
    - Spring은 올바른 패키지를 스캔한 후 컴포넌트를 찾아 인스턴스를 생성하며 이를 자동으로 추가해서 전체 애플리케이션을 작동하도록 한다

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
@@ -2,26 +2,17 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 // 다른 패키지에 정의된 Component 클래스를 찾는다
 @ComponentScan("com.in28minutes.learnspringframework.game")
-public class App03GamingSpringBeans {
-    @Bean
-    // 기존에 등록된 Bean 연결 하기(Wiring) ==> Auto-Wiring
-    public GameRunner gameRunner(GamingConsole gamingConsole) {
-        // 많은 GamingConsole 구현체 중에서 Component Scanning의 대상인 MarioGame을 연결해준다
-        System.out.println("Parameter: " + gamingConsole);
-        return new GameRunner(gamingConsole);
-    }
-
+public class GamingAppLauncherApplication {
     public static void main(String[] args) {
         try(AnnotationConfigApplicationContext context
                     = new AnnotationConfigApplicationContext
-                (App03GamingSpringBeans.class)) {
+                (GamingAppLauncherApplication.class)) {
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
