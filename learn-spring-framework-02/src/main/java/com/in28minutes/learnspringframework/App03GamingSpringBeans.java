ff9ccd611e932f6548f1790131918d4b592e1bff
tataki26 <tataki26@gmail.com>
2023-08-28 17:32:54 +0900

    Java 객체 생성 및 관리를 위한 Spring 프레임워크 이해하기 2
    - App 클래스와 Configuration 클래스 합치기
    - @Configuration과 @Bean을 App 클래스에 정의

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -2,12 +2,26 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class App03GamingSpringBeans {
+    @Bean
+    public GamingConsole gamingConsole() {
+        return new MarioGame();
+    }
+
+    @Bean
+    // 기존에 등록된 Bean 연결 하기(Wiring)
+    public GameRunner gameRunner(GamingConsole gamingConsole) {
+        return new GameRunner(gamingConsole);
+    }
+
     public static void main(String[] args) {
         try(AnnotationConfigApplicationContext context
                     = new AnnotationConfigApplicationContext
-                (GamingConfiguration.class)) {
+                (App03GamingSpringBeans.class)) {
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
