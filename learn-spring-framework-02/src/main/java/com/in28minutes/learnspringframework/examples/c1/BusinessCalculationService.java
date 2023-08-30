80664d4e19917dc848a17a40102464ab141a31c2
tataki26 <tataki26@gmail.com>
2023-08-30 23:06:48 +0900

    Spring 프레임워크 스테레오 타입 어노테이션 - Component 등
    - @Service: 비즈니스 로직을 처리하는 클래스
    - @Controller: 웹 컨트롤러 클래스
    - @Repository: 데이터베이스와 상호 작용(통신)하는 클래스
    - 그 외 클래스는 @Component로 표현
    
    - 되도록이면 구체적인 어노테이션을 사용할 것
    - 자신이 의도했던 바를 더 자세히 나타낼 수 있다
    - AOP(관점 지향 프로그래밍)를 사용하여 어노테이션을 감지하고 그 위에 부가적인 동작을 추가할 수 있다(ex:) @Repository가 있으면 자동으로 JDBC 예외 변환 기능에 연결)

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/c1/BusinessCalculationService.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/c1/BusinessCalculationService.java
@@ -2,10 +2,13 @@ package com.in28minutes.learnspringframework.examples.c1;
 
 import com.in28minutes.learnspringframework.examples.c1.data.DataService;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
-@Component
+// @Component
+// 비즈니스 로직을 다루는 컴포넌트
+@Service
 public class BusinessCalculationService {
     private DataService dataService;
 
--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/c1/data/MongoDbDataService.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/c1/data/MongoDbDataService.java
@@ -2,11 +2,14 @@ package com.in28minutes.learnspringframework.examples.c1.data;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+// @Component
 // 구현체가 두 개이므로 우선권을 부여해야 한다
 // 그렇지 않으면, no unique 관련 예외가 발생한다
 @Primary
+// 데이터베이스와 상호 작용(통신)하는 컴포넌트
+@Repository
 public class MongoDbDataService implements DataService {
     @Override
     public int[] retrieveData() {
