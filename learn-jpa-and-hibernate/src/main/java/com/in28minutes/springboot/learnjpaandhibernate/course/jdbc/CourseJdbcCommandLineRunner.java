57a204ba58042d2a27100c78444a93177febf1ab
tataki26 <tataki26@gmail.com>
2023-09-04 11:41:16 +0900

    Spring JDBC를 사용하여 데이터 삽입 및 삭제하기 1
    * 쿼리의 값을 하드 코딩하지 않고 인자로 넘기기
    - jdbc 메서드에 파라미터로 값을 넘길 수 있다
    - 이때, 쿼리에는 파싱 할 데이터를 '?'로 둔다
    - ** 인자의 순서를 꼭 지켜야 한다 **

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.jdbc.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,8 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Learn AWS Now!", "in28minutes"));
+        repository.insert(new Course(2, "Learn Azure Now!", "in28minutes"));
+        repository.insert(new Course(3, "Learn DevOps Now!", "in28minutes"));
     }
 }
--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.jdbc.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,10 +13,10 @@ public class CourseJdbcRepository {
     private static String INSERT_QUERY =
             """
                 insert into course (id, name, author)
-                values(1, 'Learn AWS', 'in28minutes');
+                values(?,?,?);
             """;
 
-    public void insert() {
-        springJdbcTemplate.update(INSERT_QUERY);
+    public void insert(Course course) {
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
     }
 }
