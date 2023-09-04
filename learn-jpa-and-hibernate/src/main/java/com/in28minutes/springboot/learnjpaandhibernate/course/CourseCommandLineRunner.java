0c299efa66339ea615daaac8604d3da780e454cd
tataki26 <tataki26@gmail.com>
2023-09-04 15:58:29 +0900

    Spring Data JPA 시작하기
    * Spring Data JPA
    - EntityManager 생략 가능
    - JpaRepository 인터페이스를 상속 받는 Repository 인터페이스만 정의해도 Spring Data JPA가 제공하는 여러 메서드를 사용할 수 있다
    - 제네릭에는 매핑할 객체 타입과 PK의 타입을 명시한다
    - EntityManager는 백그라운드에서 처리
    - 코드가 한 층 더 간결해진다
    
    - insert >> save
    - batch, findAll, existById, ...

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.learnjpaandhibernate.course;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,17 +11,18 @@ import org.springframework.stereotype.Component;
 public class CourseCommandLineRunner implements CommandLineRunner {
     @Autowired
     // private CourseJdbcRepository repository;
-    private CourseJpaRepository repository;
+    // private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS JPA!", "in28minutes"));
-        repository.insert(new Course(2, "Learn Azure JPA!", "in28minutes"));
-        repository.insert(new Course(3, "Learn DevOps JPA!", "in28minutes"));
+        repository.save(new Course(1, "Learn AWS JPA!", "in28minutes"));
+        repository.save(new Course(2, "Learn Azure JPA!", "in28minutes"));
+        repository.save(new Course(3, "Learn DevOps JPA!", "in28minutes"));
 
-        repository.deleteById(1);
+        repository.deleteById(1l);
 
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        System.out.println(repository.findById(2l));
+        System.out.println(repository.findById(3l));
     }
 }
