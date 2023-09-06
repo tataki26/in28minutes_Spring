5b0a485b7a4964b10ae784bda244ba177479a1fa
tataki26 <tataki26@gmail.com>
2023-09-06 15:53:59 +0900

    로그인 양식 만들기
    - 요구 사항: 사용자 ID와 패스워드 받기
    
    - 요청 파라미터가 아닌 view의 form 태그를 통해 데이터 받기
    
    - 메서드를 설정하지 않으면 GET으로 요청 전송
    - 사용자가 입력한 데이터가 URL에 쿼리 파라미터로 노출된다
    - 사용자 정보를 응답 Body로 넘기기 위해서는 POST 메서드를 사용해야 한다

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -3,22 +3,14 @@ package com.in28minutes.springboot.myfirstwebapp.login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("login")
-    // http://localhost:8080/login?name=Ranga
-    // Model: view에 데이터를 넘기는 객체
-    public String goToLoginPage(@RequestParam String name, ModelMap model) {
-        // jsp의 name이라는 이름의 속성에 name 파라미터 전달
-        model.put("name", name);
-        logger.debug("Request param is {}", name);
-        logger.info("I want this printed at info level");
+    public String goToLoginPage() {
         return "login";
     }
 }
