72c2d6d755327d8bd25d17d8f42c4610354a312d
tataki26 <tataki26@gmail.com>
2023-09-06 10:04:14 +0900

    빠른 개요 - Spring Boot를 사용할 때 로깅의 중요성
    - 프로덕션 중에 문제를 디버깅 할 때 로그 확인은 필수!
    
    - application.properties에서 로깅 레벨 설정 가능
    - logging.level.패키지명=로깅레벨
    - debug는 자세하지만 성능에 영향을 많이 미치므로 애플리케이션 실행 중에는 info가 적절하다
    - 특정 패키지 명을 설정함으로써 패키지 별로 로깅 레벨 설정이 가능하다
    
    - Logger 클래스 사용(LoggerFactory.getLogger(getClass()))
    - slf4j 프레임워크의 Logback
    - logger.info / logger.debug / ...
    
    - 종속성: spring-boot-starter-web의 하위 ...-starter-logging

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +9,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("login")
     // http://localhost:8080/login?name=Ranga
     // Model: view에 데이터를 넘기는 객체
     public String goToLoginPage(@RequestParam String name, ModelMap model) {
         // jsp의 name이라는 이름의 속성에 name 파라미터 전달
         model.put("name", name);
-        // 예시로 사용하는 것일 뿐 실무에서는 콘솔 출력 대신 로깅 사용을 권장
-        System.out.println("Request param is " + name);
+        logger.debug("Request param is {}", name);
+        logger.info("I want this printed at info level");
         return "login";
     }
 }
