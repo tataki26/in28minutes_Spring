f58a09d94490343ecdf4a842269606cf8306b555
tataki26 <tataki26@gmail.com>
2023-09-06 09:50:52 +0900

    RequestParam으로 쿼리 파라미터 잡기, 모델 소개
    * 쿼리 파라미터
    - url에 특정 데이터를 포함해서 서버에 전송하는 수단
    - @RequestParam: controller 메서드의 파라미터 앞에 붙이면 쿼리 파라미터를 받을 수 있다
    
    * model
    - view에 데이터를 전송하기 위한 객체
    - .put: 인자로 view 속성 이름과 파라미터 이름을 넘긴다
    
    * JSP 표현 언어
    - ${변수 이름}: model 객체의 데이터 사용
    
    - ** 실무에서는 콘솔 출력 대신 로깅 사용을 권장한다 **

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,12 +1,20 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     @RequestMapping("login")
-    public String goToLoginPage() {
+    // http://localhost:8080/login?name=Ranga
+    // Model: view에 데이터를 넘기는 객체
+    public String goToLoginPage(@RequestParam String name, ModelMap model) {
+        // jsp의 name이라는 이름의 속성에 name 파라미터 전달
+        model.put("name", name);
+        // 예시로 사용하는 것일 뿐 실무에서는 콘솔 출력 대신 로깅 사용을 권장
+        System.out.println("Request param is " + name);
         return "login";
     }
 }
