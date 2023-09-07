c08019f7ccc23a05665bc87d1105c390b7b4ae47
tataki26 <tataki26@gmail.com>
2023-09-07 15:45:24 +0900

    Todo 추가하기 - 새로운 뷰 만들기
    - 요구 사항: Add Todo 버튼과 todo를 추가하는 페이지를 만들고 새로운 todo를 제출하면 todo list 페이지로 리다이렉션 하기
    
    - 리다이렉션을 하려면 컨트롤러 메서드의 반환값으로 "redirect:리다이렉트 할 url 엔드포인트"를 넘기면 된다

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
@@ -21,5 +22,17 @@ public class TodoController {
         List<Todo> todos = todoService.findByUsername("in28minutes");
         model.addAttribute("todos", todos);
         return "listTodos";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+    public String showNewTodoPage() {
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+    public String addNewTodo() {
+        // list-todos 페이지로 리다이렉트
+        // view 이름이 아닌 url을 적어야 한다
+        return "redirect:list-todos";
     }
 }
