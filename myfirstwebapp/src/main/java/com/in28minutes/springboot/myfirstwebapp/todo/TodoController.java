cf0f6f573c926f90c682cd76aabc4f02196d5043
tataki26 <tataki26@gmail.com>
2023-09-07 16:00:09 +0900

    Todo를 추가하기 위해 TodoService 개선하기
    - 요구 사항: 사용자가 입력한 데이터를 가져와서 서버 내부 List에 항목 추가하기
    
    - @RequestParam을 통해 사용자가 form에 입력한 데이터 가져오기
    - 추가 로직은 Service에서 만들고 Controller는 해당 메서드 호출
    - 목록에 항목이 추가 될때마다 count를 증가 시켜 id로 사용
    - 사용자가 로그인 페이지에서 입력한 username을 가져오기 위해 model.get(속성) 사용

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -30,7 +32,9 @@ public class TodoController {
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodo() {
+    public String addNewTodo(@RequestParam String description, ModelMap model) {
+        String username = (String)model.get("name");
+        todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
         // list-todos 페이지로 리다이렉트
         // view 이름이 아닌 url을 적어야 한다
         return "redirect:list-todos";
