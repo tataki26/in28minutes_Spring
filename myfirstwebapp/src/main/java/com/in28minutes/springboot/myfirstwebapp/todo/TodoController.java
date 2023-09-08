31c4dc189244a5723e57331cec9a23c9b2cdb77a
tataki26 <tataki26@gmail.com>
2023-09-08 09:40:25 +0900

    Spring Boot Starter Validation을 이용하여 검증 추가하기
    - required="required": 해당 필드가 null이면 에러 메시지를 화면에 표시(해당 필드 값은 필수적)
    
    - 프론트엔드의 HTML과 JS로 구현한 검증은 해커에 의해 쉽게 뚫릴 수 있다
    - 따라서, 서버 측 검증이 필수적이다
    
    * 서버 검증의 네 가지 단계
    - 검증과 관련된 프로젝트 임포트(spring-boot-starter-validation)
    - 커맨드 빈(양식 보조 객체)을 통한 양방향 바인딩 구현
    - Bean에 검증 추가
    - 검증 오류를 뷰에 표시
    
    * 커맨드 빈(양식 보조 객체)
    - 사용자 입력을 @RequestParam으로 받지 않고 Bean에 직접 바인딩 하는 방법
    - ModelMap을 첫번째 파라미터로 지정하고 그 다음에 커맨드 빈을 추가한다
    - jsp에서 양식 보조 객체를 위한 라이브러리(form tag library)를 import한다
    - 속성과 Bean을 연결한다(modelAttribute=파라미터로 넘긴 객체 이름)
    - 특정 필드와 매핑하기 위해 path를 추가한다
    - model에 todo 속성을 추가한다(new)
    
    * 숨은 변수
    - 입력 변수가 null인 필드들에 대해서 path를 지정하고 hidden이라는 필드를 추가하면 화면에 해당 필드들에 대한 초기 값을 감출 수 있다

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -2,10 +2,7 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -27,14 +24,19 @@ public class TodoController {
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.GET)
-    public String showNewTodoPage() {
+    public String showNewTodoPage(ModelMap model) {
+        String username = (String)model.get("name");
+        Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+        model.put("todo", todo);
         return "todo";
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodo(@RequestParam String description, ModelMap model) {
+    // command bean / 양식 보조 객체
+    // 사용자 입력을 Bean에 직접 바인딩하기
+    public String addNewTodo(ModelMap model, Todo todo) {
         String username = (String)model.get("name");
-        todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+        todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
         // list-todos 페이지로 리다이렉트
         // view 이름이 아닌 url을 적어야 한다
         return "redirect:list-todos";
