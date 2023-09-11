789c31fff02747e67a77592a63fb530e57ede6d2
tataki26 <tataki26@gmail.com>
2023-09-11 12:17:20 +0900

    H2 콘솔을 사용하기 위해 Spring Security 설정하기
    - Spring Security를 사용할 시, 별도의 설정을 하지 않으면 모든 URL은 보호되며 승인 되지 않은 요청에 대해서는 로그인 양식이 표시된다
    - 또한, db에 접속하기 위해서는 사이트 간 요청 위조(CSRF) 기능을 비활성화 해야 한다(현재 http.csrf()는 deprecated 됐다)
    - 마지막으로 Spring Security는 HTML과 H2 웹사이트가 사용하는 프레임을 허용하지 않으므로 프레임을 허용하도록 설정해야 한다(http.headers().frameOptions()도 deprecated 됐다)

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,13 +2,19 @@ package com.in28minutes.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -39,5 +45,27 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // All URLs are protected
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+
+        // A login form is shown for unauthorized request
+        http.formLogin(withDefaults());
+
+        // CSRF disable
+        // http.csrf().disable(); // deprecated
+        // 서로 무관한 함수들도 직렬로 이어지는 메서드 체이닝을 지양하고
+        // 함수형으로 수납하는 형태를 지향
+        http.csrf(c -> c.disable()); // CSRF 설정을 위한 함수
+
+        // Frames
+        // http.headers().frameOptions().disable(); // deprecated
+        http.headers(c -> c.frameOptions(f -> f.disable()).disable());
+
+        return http.build();
     }
 }
