d1a0c96e914e3753307c0e6181e0e122c2170dac
tataki26 <tataki26@gmail.com>
2023-09-12 22:52:45 +0900

    User Resource에서 GET 메서드 구현하기
    - User와 관련된 요청 처리를 위한 UserResource 컨트롤러 추가
    - 데이터베이스(List)와 연동하는 UserDaoService에 의존
    
    - 전체 사용자 정보 조회 하기
    - 쿼리 파라미터로 특정 사용자 정보 조회(findById) 하기
    
    - 컨트롤러 메서드에서 데이터베이스 연동 메서드 호출

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,26 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findById(id);
+    }
+}
