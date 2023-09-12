ea4d08396c083347d6e5a94096dca5cbbd90f7dd
tataki26 <tataki26@gmail.com>
2023-09-12 23:05:04 +0900

    User Resource에서 POST 메서드 구현하기
    - POST 메서드로 사용자 정보 추가 요청
    - @RequestBody로 User 정보(json) 담아서 요청하기
    
    - id는 ++userCount를 통해 자동 할당
    - 클라이언트로부터 받은 User 객체를 List에 넣고(add) Id는 setter를 통해 직접 할당
    
    - ** 브라우저로는 GET 요청만 확인 가능, 그 외 요청은 REST API 클라이언트 필요 **

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,10 @@ public class UserResource {
     @GetMapping(path = "/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findById(id);
+    }
+
+    @PostMapping(path = "/users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
     }
 }
