88ca222c9ecdb2329db3b4b63376f5898972de28
tataki26 <tataki26@gmail.com>
2023-09-13 13:42:42 +0900

    REST API에서 유효성 검증하기
    - 검증하고 싶은 인자 앞에 @Valid 추가
    - 프로퍼티나 메서드 인자, 반환 타입의 유효성 검증
    - 바인딩 시. 객체에 정의된 유효성 검증이 자동으로 수행된다
    
    - @Size: 필드 크기
    - @Past: 과거 날짜
    - jakarta.validation.constraints에서 다양한 제약 조건을 위한 어노테이션을 확인할 수 있다
    
    - handleMethodArgumentNotValid라는 메서드를 오버라이딩 하여 MethodArgumentNotValidException(유효하지 않은 인자)을 처리할 수 있다
    - 그 외에도 헤더, 상태 코드, 웹 요청을 인자로 받는다
    - 응답 상태를 지정할 수 있다
    - ex.getErrorCount()를 통해 에러 개수를 알 수 있고 getFieldError().getDefaultMessage()를 통해 구체적인 에러를 알 수 있다

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -32,7 +33,7 @@ public class UserResource {
     }
 
     @PostMapping(path = "/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest() // get current request URL
                         .path("/{id}") // add path
