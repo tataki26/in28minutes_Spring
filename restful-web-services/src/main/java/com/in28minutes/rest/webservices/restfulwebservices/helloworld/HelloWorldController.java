e8ea08daf186111988ca93f5f12e8c4f90f7876a
tataki26 <tataki26@gmail.com>
2023-09-13 17:08:33 +0900

    REST API의 국제화 알아보기
    - HTTP 요청 헤더 중 Accept-Language는 국제화용 헤더이다
    - 자연어와 소비자가 선호하는 locale을 나타낸다
    
    - 국제화 설정은 messages_(국가별).properties에서 정의한다
    - 국가별로 각 properties 파일에서 동일한 key를 정의한다
    
    - 사용자가 요청한 locale을 가져오기 위해서는 LocaleContextHolder.getLocale()을 사용한다
    - 현재 스레드와 연관된 locale을 가져온다
    
    - MessageSource: 메시지의 매개변수화나 국제화에 대한 지원을 통해 메시지를 처리하는 전략 인터페이스
    - 이 인터페이스를 통해 messages.properties의 프로퍼티를 가져올 수 있다
    - Default Message를 지정해서 사용자 요청 중 지원하는 메시지가 없을 때 기본으로 제공한다

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 // REST 컨트롤러 지정
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -21,5 +31,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalize() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
