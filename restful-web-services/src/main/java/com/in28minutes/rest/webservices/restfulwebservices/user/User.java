56cd4af49b0c1dba6b2322fa192c7ebb86941f0b
tataki26 <tataki26@gmail.com>
2023-09-14 10:29:47 +0900

    REST API 정적 필터링 구현하기
    - 직렬화: 객체를 스트림(JSON/XML)으로 전환하는 프로세스
    - Jackson: Java의 JSON 직렬화 프레임워크
    
    - @JSONProperty: 응답 필드(JSON key) 이름 커스터마이징
    
    * 선택된 필드만 반환하기 - 필터링
    - 정적 필터링: REST API와 상관 없이 항상 특정 필드를 차단하거나 필터링하는 것, 여러 REST API에서 빈에 적용되는 동일한 필터링
    - 동적 필터링: 응답의 일부로 필드를 전송할지 동적으로 결정하는 필터링
    
    * 정적 필터링
    - @JsonIgnore: 특정 필드 필터링
    - @JsonIgnoreProperties: 빈에 지정, 프로퍼티 이름을 넘겨 필터링 대상 지정
    - @JsonIgnore 사용을 권장한다

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,8 +9,11 @@ import java.time.LocalDate;
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters")
+    // json key 이름 변경
+    @JsonProperty("user_name")
     private String name;
     @Past(message = "Birth Date should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
