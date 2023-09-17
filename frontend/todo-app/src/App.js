1f662395962a94551e7bdf4cc90a8bf7d7b59282
tataki26 <tataki26@gmail.com>
2023-09-17 15:38:32 +0900

    JavaScript 모범 사례 따라 하기 - 모듈로 리팩토링 2
    - 기존 App에서 호출했던 각각의 컴포넌트를 LearningComponent를 정의해서 대신 호출하도록 변경
    - App.js의 App은 LearningComponent만을 호출하면 되므로 매우 깔끔해지며 같은 폴더 안에서 정의되므로 import 경로도 간결해진다

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import FirstComponent from './components/learning-examples/FirstComponent';
-import SecondComponent from './components/learning-examples/SecondComponent';
-import ThirdComponent from './components/learning-examples/ThirdComponent';
-import FourthComponent from './components/learning-examples/FourthComponent';
-import { FifthComponent } from './components/learning-examples/FirstComponent';
+
+import LearningComponent from './components/learning-examples/LearningComponent';
 
 function App() {
   return (
     <div className="App">
-      <FirstComponent />
-      <SecondComponent></SecondComponent>
-      <ThirdComponent />
-      <FourthComponent></FourthComponent>
-      <FifthComponent />
+      <LearningComponent />
     </div>
   );
 }
