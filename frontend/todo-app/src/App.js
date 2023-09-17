96d61a49d9470e06bba3934af8e51df58d00be5a
tataki26 <tataki26@gmail.com>
2023-09-17 17:00:14 +0900

    React 애플리케이션 시작하기 - Counter
    - Counter 애플리케이션을 위한 Counter 함수형 컴포넌트 생성
    - return으로 최종 반환될 JSX를 정의한다
    - 함수 내부에 counter 로직을 처리하는 함수를 정의할 수 있다(incrementCounterFunction)
    
    - onClick 이벤트와 함수를 연결할 때는 {함수명}을 하면 된다(함수 끝에 괄호를 붙일 필요 없다 - 호출)

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-import LearningComponent from './components/learning-examples/LearningComponent';
+import Counter from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <LearningComponent />
+      <Counter />
     </div>
   );
 }
