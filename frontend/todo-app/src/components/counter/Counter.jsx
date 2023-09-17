900f137384143f235d7083b5f5b82050b2972927
tataki26 <tataki26@gmail.com>
2023-09-17 20:29:56 +0900

    React State 알아 보기 - 백그라운드에서는 무슨 일이 일어날까?
    - '-1' 기능 추가
    
    * 리액트 백그라운드에서 일어나는 일
    - 상태만 업데이트 해도 뷰가 자동으로 업데이트 된다
    - HTML 페이지는 일반적으로 DOM(문서 객체 모델) 요소로 표현된다
    - HTML의 각 요소는 DOM 노드에 해당(계층 구조)하고 요소를 업데이트 하려면 DOM을 업데이트 해야 한다
    - DOM 업데이트 코드를 작성하는 것은 매우 복잡하고 시간이 걸린다
    - 특정 값을 바꾸려면 해당 값이 있는 요소의 위치를 찾아내는 JavaScript 코드를 작성해야 한다
    - 그러나 리액트는 가상 돔(virtual DOM)이라는 개념을 사용해서 상태가 업데이트 될 때마다 가상 DOM을 업데이트하고 이전 가상 DOM과의 차이점만 실제 DOM에 동기화 시킨다
    - 따라서, 직접 DOM을 업데이트 할 필요가 없다

--- frontend/todo-app/src/components/counter/Counter.jsx
+++ frontend/todo-app/src/components/counter/Counter.jsx
@@ -2,18 +2,17 @@ import { useState } from 'react';
 import './Counter.css';
 
 export default function Counter() {
-    // const state = useState(0);
     // 구조 분해
     // current value, function(for update)
     // useStatue(initial value);
     const [count, setCount] = useState(0);
 
     function incrementCounterFunction() {
-        // state[1](state[0] + 1);
-        // console.log(state[0]);
-        // console.log(state[1]);
         setCount(count + 1);
-        // console.log(count);
+    }
+
+    function decrementCounterFunction() {
+        setCount(count - 1);
     }
 
     return (
@@ -23,6 +22,9 @@ export default function Counter() {
                 <button className="counterButton" 
                         onClick={incrementCounterFunction}
                         >+1</button>
+                <button className="counterButton"
+                        onClick={decrementCounterFunction}
+                        >-1</button>
             </div>
         </div>
     )
