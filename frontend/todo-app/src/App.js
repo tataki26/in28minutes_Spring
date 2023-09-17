6c4cd6e9b9e152137bc471fb94b0be7c4d66610d
tataki26 <tataki26@gmail.com>
2023-09-17 11:32:45 +0900

    첫번째 React 컴포넌트 생성 등
    - src/App.js: 페이지에 로드되는 실제 내용이 들어있는 파일
    - 모든 커스텀 리액트 컴포넌트는 앱 컴포넌트의 자식 컴포넌트
    
    * 함수형 컴포넌트
    - function 함수명() { return() }
    - return의 괄호 안에 반환하려는 항목 정의(JSX)
    - 앱 컴포넌트에서 해당 함수 호출(태그)
    
    * 클래스 컴포넌트
    - class 클래스명 extends Component { render() {return () } }
    - React 프레임워크가 제공하는 Component를 import 한다
    - render라는 메서드를 사용한다(랜더링 메서드)
    - 마찬가지로 return에서 반환하려는 항목을 정의한다

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
-      My Todo Application
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <FourthComponent></FourthComponent>
     </div>
   );
+}
+
+// function component
+
+function FirstComponent() {
+  return (
+    <div className="FirstComponent">First Component</div>
+  )
+}
+
+function SecondComponent() {
+  return (
+    <div className='SecondComponent'>Second Component</div>
+  )
+}
+
+// class component
+
+class ThirdComponent extends Component {
+  render() {
+    return (
+      <div className='ThirdComponent'>Third Component</div>
+    )
+  }
+}
+
+class FourthComponent extends Component {
+  render() {
+    return (
+      <div className='FourthComponent'>Fourth Component</div>
+    )
+  }
 }
 
 export default App;
