783ee27ea2355c2dc2ef2d2b22edc46d8a7eff6c
tataki26 <tataki26@gmail.com>
2023-09-18 15:11:51 +0900

    하드 코딩으로 인증 추가 - Todo React 앱
    - 인증 성공/실패에 대한 state를 추가하고 로그인에 성공하면 성공(true)/실패(false)를, 실패하면 성공(false)/실패(true)를 설정한다
    - 해당 로직을 login 버튼에 대한 이벤트 핸들러로 등록한다
    
    - 성공/실패 메시지 컴포넌트를 정의하여 state 값에 따라 관련 메시지를 담은 JSX를 반환한다
    
    - state에 따라 UI를 업데이트하는 방법 실습

--- frontend/todo-app/src/components/todo/ToDoApp.jsx
+++ frontend/todo-app/src/components/todo/ToDoApp.jsx
@@ -14,6 +14,9 @@ function LoginComponent() {
    const [username, setUsername] = useState('in28minutes')
    const [password, setPassword] = useState('')
 
+   const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+   const [showErrorMessage, setShowErrorMessage] = useState(false)
+
    function handleUsernameChange(event) {
         setUsername(event.target.value);
    }
@@ -23,8 +26,36 @@ function LoginComponent() {
         setPassword(event.target.value);
    }
 
+   function handleSubmit() {
+        if(username === 'in28minutes' && password === 'dummy') {
+            console.log('Success');
+            setShowSuccessMessage(true);
+            setShowErrorMessage(false);
+        } else {
+            console.log('Failed');
+            setShowSuccessMessage(false);
+            setShowErrorMessage(true);
+        }
+   }
+
+   function SuccessMessageComponent() {
+        if (showSuccessMessage) {
+            return <div className="successMessage">Authenticated Successfully</div>
+        }
+        return null
+    }
+
+    function ErrorMessageComponent() {
+        if (showErrorMessage) {
+            return <div className="errorMessage">Authenticated Failed. Please check your credentials.</div>
+        }
+        return null
+    }
+
     return (
         <div className="Login">
+            <SuccessMessageComponent />
+            <ErrorMessageComponent />
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
@@ -35,7 +66,7 @@ function LoginComponent() {
                     <input type="password" name="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <div>
-                    <button type="button" name="login">login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>login</button>
                 </div>
             </div>
         </div>
