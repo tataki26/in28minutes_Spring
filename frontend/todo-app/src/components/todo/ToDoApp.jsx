fb45a2c5f646e6700a51ae4b1bf8f6f1b8770b53
tataki26 <tataki26@gmail.com>
2023-09-18 15:24:08 +0900

    로그인 컴포넌트에서 조건에 따른 메시지 표시 - Todo React 앱
    * JavaScript 표현식
    - true && value ==> value
    - false && value ==> false
    - 이 표현식을 JSX에 사용해서 값이 ture일 때만 특정 요소를 보여주도록 설정 할 수 있다
    
    - 기존에 사용하던 메서드를 제거하고 표현식 적용

--- frontend/todo-app/src/components/todo/ToDoApp.jsx
+++ frontend/todo-app/src/components/todo/ToDoApp.jsx
@@ -36,26 +36,12 @@ function LoginComponent() {
             setShowSuccessMessage(false);
             setShowErrorMessage(true);
         }
-   }
-
-   function SuccessMessageComponent() {
-        if (showSuccessMessage) {
-            return <div className="successMessage">Authenticated Successfully</div>
-        }
-        return null
-    }
-
-    function ErrorMessageComponent() {
-        if (showErrorMessage) {
-            return <div className="errorMessage">Authenticated Failed. Please check your credentials.</div>
-        }
-        return null
     }
 
     return (
         <div className="Login">
-            <SuccessMessageComponent />
-            <ErrorMessageComponent />
+            {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
+            {showErrorMessage && <div className="errorMessage">Authenticated Failed. Please check your credentials.</div>}
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
