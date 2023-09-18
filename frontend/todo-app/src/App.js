f64221f36b499abc3dde0c7cd56b06e45d5b6ac6
tataki26 <tataki26@gmail.com>
2023-09-18 13:56:03 +0900

    로그인 컴포넌트 시작하기 - Todo React 앱
    - 상위 컴포넌트: ToDoApp
    - 하위 컴포넌트: LoginComponent / WelcomeComponent
    
    - LoginComponent 내부에 LoginForm 정의(User Name, Password, login button)
    
    - JSX 주석: { /* */ }, ctrl + /

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Counter from './components/counter/Counter';
+import TodoApp from './components/todo/ToDoApp'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <TodoApp />
     </div>
   );
 }
