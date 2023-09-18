6a431cee8147e9e777998d079e6350a305345d47
tataki26 <tataki26@gmail.com>
2023-09-18 16:45:17 +0900

    React 앱에 에러 컴포넌트 추가하기
    - Route의 path를 '/*'로 설정하면 존재하지 않는 URL(설정하지 않은 route)에 대해서 컴포넌트를 매핑 할 수 있다(ex:) 404 에러 페이지)
    - ErrorComponent 추가

--- frontend/todo-app/src/components/todo/ToDoApp.jsx
+++ frontend/todo-app/src/components/todo/ToDoApp.jsx
@@ -10,6 +10,7 @@ export default function TodoApp() {
                     <Route path='/' element={ <LoginComponent /> }></Route>
                     <Route path='/login' element={ <LoginComponent /> }></Route>
                     <Route path='/welcome' element={ <WelcomeComponent /> }></Route>
+                    <Route path='/*' element={ <ErrorComponent /> }></Route>
                 </Routes>
             </BrowserRouter>
         </div>
@@ -48,6 +49,7 @@ function LoginComponent() {
 
     return (
         <div className="Login">
+            <h1>Time to Login!</h1>
             {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
             {showErrorMessage && <div className="errorMessage">Authenticated Failed. Please check your credentials.</div>}
             <div className="LoginForm">
@@ -69,8 +71,22 @@ function LoginComponent() {
 
 function WelcomeComponent() {
     return (
-        <div className="Welcome">
-            Welcome Component
+        <div className="WelcomeComponent">
+            <h1>Welcome in28minutes</h1>
+            <div className="Welcome">
+                Welcome Component
+            </div>
+        </div>
+    )
+}
+
+function ErrorComponent() {
+    return (
+        <div className="ErrorComponent">
+            <h1>We are working really hard!</h1>
+            <div>
+                Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.
+            </div>
         </div>
     )
 }
