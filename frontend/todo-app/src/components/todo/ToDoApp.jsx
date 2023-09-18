c7bc0f3a79ca2409996a35e2c9c7ac3132d27645
tataki26 <tataki26@gmail.com>
2023-09-18 17:01:53 +0900

    Welcome 컴포넌트에서 하드 코딩 삭제
    - Welcome 컴포넌트에 사용자가 입력한 값을 가져와서 사용하기
    
    - welcome path에 경로 파라미터 추가(/:경로 파라미터)
    - useParams: 라우팅 경로에 해당하는 url에서 동적 파라미터인 key-value 쌍을 객체로 반환하는 hook(경로 파라미터 반환)
    - 이때, useParams의 객체가 아닌 속성 값을 직접 받고 싶으면 '{ 속성 }'으로 표기하면 된다
    
    - JSX에서는 {}에, JavaScript에서는 `${}`에 변수를 받는다
    
    * 순서 정리
    1. 로그인 시, 사용자가 입력한 username이 url의 경로 파라미터로 전달된다
    2. welcome 컴포넌트에서 해당 username을 받아서 JSX에 동적으로 값을 표현한다

--- frontend/todo-app/src/components/todo/ToDoApp.jsx
+++ frontend/todo-app/src/components/todo/ToDoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate, useParams } from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
@@ -9,7 +9,7 @@ export default function TodoApp() {
                 <Routes>
                     <Route path='/' element={ <LoginComponent /> }></Route>
                     <Route path='/login' element={ <LoginComponent /> }></Route>
-                    <Route path='/welcome' element={ <WelcomeComponent /> }></Route>
+                    <Route path='/welcome/:username' element={ <WelcomeComponent /> }></Route>
                     <Route path='/*' element={ <ErrorComponent /> }></Route>
                 </Routes>
             </BrowserRouter>
@@ -39,7 +39,7 @@ function LoginComponent() {
             console.log('Success');
             setShowSuccessMessage(true);
             setShowErrorMessage(false);
-            navigate('/welcome')
+            navigate(`/welcome/${username}`)
         } else {
             console.log('Failed');
             setShowSuccessMessage(false);
@@ -70,9 +70,14 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
+    // const params = useParams()
+    const { username } = useParams()
+
+    console.log(username)
+
     return (
         <div className="WelcomeComponent">
-            <h1>Welcome in28minutes</h1>
+            <h1>Welcome {username}</h1>
             <div className="Welcome">
                 Welcome Component
             </div>
