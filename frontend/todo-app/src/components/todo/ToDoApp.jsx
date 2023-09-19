9e3530ed18fe78bea61925531d257cd6429f78bc
tataki26 <tataki26@gmail.com>
2023-09-19 23:48:05 +0900

    헤더, 바닥글, 로그아웃 컴포넌트 React로 만들기
    - 헤더, 바닥글, 로그아웃 컴포넌트 추가
    
    - 헤더와 바닥글은 TodoApp 컴포넌트에 추가하여 모든 페이지에 동일하게 나타나도록 설정
    - 이때, 다른 페이지로 넘어 가도 새로 고침이 되지 않고 오직 변경된 컨텐츠만 새로 고침된다
    - hr 태그: 수평선

--- frontend/todo-app/src/components/todo/ToDoApp.jsx
+++ frontend/todo-app/src/components/todo/ToDoApp.jsx
@@ -5,16 +5,21 @@ import './TodoApp.css'
 export default function TodoApp() {
     return (
         <div className="TodoApp">
+            <HeaderComponent />
+
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={ <LoginComponent /> } />
                     <Route path='/login' element={ <LoginComponent /> } />
                     <Route path='/welcome/:username' element={ <WelcomeComponent /> } />
                     <Route path='/todos' element={ <ListTodosComponent /> } />
+                    <Route path='/logout' element={ <LogoutComponent /> } />
 
                     <Route path='/*' element={ <ErrorComponent /> } />
                 </Routes>
             </BrowserRouter>
+
+            <FooterComponent />
         </div>
     )
 }
@@ -137,6 +142,33 @@ function ListTodosComponent() {
                     }
                     </tbody>
                 </table>
+            </div>
+        </div>
+    )
+}
+
+function HeaderComponent() {
+    return (
+        <div className="header">
+            Header <hr/>
+        </div>
+    )
+}
+
+function FooterComponent() {
+    return (
+        <div className="footer">
+            <hr/> Footer 
+        </div>
+    )
+}
+
+function LogoutComponent() {
+    return (
+        <div className="LogoutComponent">
+            <h1>You are logged out!</h1>
+            <div>
+                Thank you for using our App. Come back soon!
             </div>
         </div>
     )
