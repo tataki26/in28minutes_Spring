4ab04487f2879fa4aaaac442199d8984a660a250
tataki26 <tataki26@gmail.com>
2023-09-20 14:03:35 +0900

    React State를 업데이트하고 인증 컨텍스를 통해 확인
    - HeaderComponent에서 useContext를 직접 호출하지 않고 AuthContext에서 이를 감싼 useAuth라는 hook을 생성하여 호출하도록 구조 변경
    
    - setInveral: 두번째 인자로 지정된 time 때마다 로직 실행(ex:) 값을 1씩 증가하는 로직)
    
    - ** React 컨텍스트의 특성 중 하나는 특정 컨텍스트가 하위 컴포넌트로 전파된다는 것이다 **
    - 따라서, HeaderComponent에서 useContext를 호출하면 그 하위 컴포넌트에서 동일한 AuthContext의 state(number)를 공유한다

--- frontend/todo-app/src/components/todo/HeaderComponent.jsx
+++ frontend/todo-app/src/components/todo/HeaderComponent.jsx
@@ -1,9 +1,8 @@
-import { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { AuthContext } from './security/AuthContext';
+import { useAuth } from './security/AuthContext'
 
 function HeaderComponent() {
-    const authContext = useContext(AuthContext)
+    const authContext = useAuth()
     console.log(authContext.number);
 
     return (
--- frontend/todo-app/src/components/todo/security/AuthContext.js
+++ frontend/todo-app/src/components/todo/security/AuthContext.js
@@ -1,12 +1,15 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
 
 // Create a Context
 export const AuthContext = createContext()
+export const useAuth = () => useContext(AuthContext)
 
 // Share the created context with other components
 export default function AuthProvider({ children }) {
     // Put some state in the context
     const [number, setNumber] = useState(10);
+
+    setInterval( () => setNumber(number + 1), 10000)
 
     return (
         <AuthContext.Provider value={ {number} }>
