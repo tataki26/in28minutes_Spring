b9d4b372665f41be79f73fe6f3868d237bebdcc7
tataki26 <tataki26@gmail.com>
2023-09-21 10:55:01 +0900

    인증 라우터로 React 라우터 보호하기 2
    - 요구 사항: 사용자가 로그인했을 때만 특정 페이지에 접근 가능하도록 제한 두기
    
    - 인증 라우터를 위한 AuthenticatedRoute 컴포넌트 추가
    - children을 prop으로 받아서 인증된 사용자인 경우에는 자식 컴포넌트를 보여주고 인증된 사용자가 아닌 경우에는 로그인 페이지로 redirection
    - 부모 컴포넌트임으로 인증이 필요한 자식 컴포넌트를 감싸는 형태로 호출

--- frontend/todo-app/src/components/todo/ToDoApp.jsx
+++ frontend/todo-app/src/components/todo/ToDoApp.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AuthProvider from './security/AuthContext'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import AuthProvider, { useAuth } from './security/AuthContext'
 import HeaderComponent from './HeaderComponent'
 import LoginComponent from './LoginComponent'
 import WelcomeComponent from './WelcomeComponent'
@@ -7,6 +7,13 @@ import ListTodosComponent from './ListTodosComponent'
 import LogoutComponent from './LogoutComponent'
 import ErrorComponent from './ErrorComponent'
 import './TodoApp.css'
+
+function AuthenticatedRoute({children}) {
+    const authContext = useAuth()
+    if (authContext.isAuthenticated)
+        return children
+    return <Navigate to="/" />
+}
 
 export default function TodoApp() {
     return (
@@ -17,9 +24,24 @@ export default function TodoApp() {
                     <Routes>
                         <Route path='/' element={ <LoginComponent /> } />
                         <Route path='/login' element={ <LoginComponent /> } />
-                        <Route path='/welcome/:username' element={ <WelcomeComponent /> } />
-                        <Route path='/todos' element={ <ListTodosComponent /> } />
-                        <Route path='/logout' element={ <LogoutComponent /> } />
+                        
+                        <Route path='/welcome/:username' element={ 
+                            <AuthenticatedRoute>
+                                <WelcomeComponent />
+                            </AuthenticatedRoute> 
+                        } />
+                        
+                        <Route path='/todos' element={ 
+                            <AuthenticatedRoute>
+                                <ListTodosComponent /> 
+                            </AuthenticatedRoute>
+                        } />
+                        
+                        <Route path='/logout' element={ 
+                            <AuthenticatedRoute>
+                                <LogoutComponent />
+                            </AuthenticatedRoute> 
+                        } />
 
                         <Route path='/*' element={ <ErrorComponent /> } />
                     </Routes>
