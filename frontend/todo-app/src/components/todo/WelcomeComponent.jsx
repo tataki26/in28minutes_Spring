c011be8e54739f7ac5497f35c85938a2b0ef91a2
tataki26 <tataki26@gmail.com>
2023-09-21 15:38:21 +0900

    Spring Boot REST API에서 Axios를 사용하는 최적의 방식
    - 요구 사항: 요청 시, 경로 변수에 값을 넣어서 전송
    
    - 경로 변수를 가진 url에 요청을 보내는 함수 추가
    - 파라미터로 username을 받고 경로 변수에 추가하기 위해 템플릿 리터럴을 사용하여 username이 포함된 url 정의
    
    - 반복되는 localhost 부분을 axios.create의 baseURL에 정의

--- frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,6 +1,6 @@
 import { useParams, Link } from 'react-router-dom'
 import { useState } from 'react'
-import { retrieveHelloWorldBean } from './api/HelloWorldApiService';
+import { retrieveHelloWorldPathVariable } from './api/HelloWorldApiService';
 
 export default function WelcomeComponent() {
     // const params = useParams()
@@ -11,15 +11,8 @@ export default function WelcomeComponent() {
     function callHelloWorldRestApi() {
         console.log('called');
 
-        // axios.get('http://localhost:8080/hello-world')
-        //     .then((response) => successfulResponse(response)
-        //     )
-        //     .catch((error) => errorResponse(error))
-        //     .finally(() => console.log('cleanup'));
-
-        retrieveHelloWorldBean()
-            .then((response) => successfulResponse(response)
-            )
+        retrieveHelloWorldPathVariable('Ranga')
+            .then((response) => successfulResponse(response))
             .catch((error) => errorResponse(error))
             .finally(() => console.log('cleanup'));
     }
