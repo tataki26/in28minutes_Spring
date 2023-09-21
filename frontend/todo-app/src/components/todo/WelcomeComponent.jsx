4ccebb7f2407b716339f84b5326c87fb155dc821
tataki26 <tataki26@gmail.com>
2023-09-21 11:37:41 +0900

    React Hello World 컴포넌트에서 Spring Boot Hello Worl REST API 호출하기
    - Welcome Component에 Call Hello World 버튼 추가
    - 버튼 클릭 시, REST API 호출
    
    - REST API 호출을 위한 Axios 라이브러리 설치
    - axios에 원하는 요청 메서드를 호출하고 url을 인자로 넘기기
    - Promise를 반환하는 axios
    - 성공 시 then에 정의된 함수 호출
    - 실패 시 catch에 정의된 함수 호출
    - 성공 여부와 무관하게 finally 호출

--- frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,8 +1,25 @@
 import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default function WelcomeComponent() {
     // const params = useParams()
     const { username } = useParams()
+
+    function callHelloWorldRestApi() {
+        console.log('called');
+        axios.get('http://localhost:8080/hello-world')
+            .then((response) => successfulResponse(response)
+            )
+            .catch((error) => errorResponse(error));
+    }
+
+    function successfulResponse(response) {
+        console.log(response);
+    }
+
+    function errorResponse(error) {
+        console.log(error);
+    }
 
     return (
         <div className="WelcomeComponent">
@@ -10,6 +27,8 @@ export default function WelcomeComponent() {
             <div className="Welcome">
                 Manage your todos - <Link to="/todos">Go here!</Link>
             </div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+                    Call Hello World</button>
         </div>
     )
 }
