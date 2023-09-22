fd05155e997c6e646af851f632fbe6a5be05a062
tataki26 <tataki26@gmail.com>
2023-09-22 09:29:22 +0900

    React 앱에서 Spring Boot REST API로부터 Todo 표시하기
    - Todo List 컴포넌트에서 백엔드 서버에서 가져온 Todo List 데이터 사용하기
    
    - TodoApiService에 TodoList를 요청하는 axios GET 함수 추가
    
    - Todo List 컴포넌트에 todos state를 추가
    - service의 axios GET 함수를 호출하고 성공 시에 setTodos에 응답 데이터를 인자로 넘기기
    
    * useEffect
    - 컴포넌트의 생명 주기에 특정 시점에서 코드를 실행하도록 돕는 hook
    - 데이터 가져오기, 구동 설정, 수동으로 DOM 조작 등의 부수 효과를 수행하는 데 사용
    - ** 코드가 랜더링 될 때마다 부수 효과로 정의한 로직 실행 **
    - 첫번째 인자는 부수 효과이고 두번째 인자는 의존성 배열이다
    - 의존성 배열에 지정된 값들 중 하나라도 변경되면 첫번째 인자가 실행된다
    - 두번째 인자를 생략하면 컴포넌트가 마운트 될 때 한번만 실행된다(컴포넌트가 다시 랜더링 시에는 부수 효과 시행X)

--- frontend/todo-app/src/components/todo/ListTodosComponent.jsx
+++ frontend/todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,13 +1,18 @@
+import { useEffect, useState } from "react";
+import { retrieveAllTodosForUsername } from "./api/TodoApiService";
+
 export default function ListTodosComponent() {
-    const today = new Date();
+    const [todos, setTodos] = useState([]);
 
-    const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDate());
+    useEffect (() => refreshTodos(), [])
 
-    const todos = [
-                    {id: 1, description: 'Learn AWS', done: false, targetDate: targetDate},
-                    {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate},
-                    {id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate},
-                ]
+    function refreshTodos() {
+        retrieveAllTodosForUsername('in28minutes')
+        .then(response => {
+            setTodos(response.data);
+        })
+        .catch(error => console.log(error));
+    }
 
     return (
         <div className="container">
@@ -30,7 +35,7 @@ export default function ListTodosComponent() {
                                     <td>{todo.id}</td>
                                     <td>{todo.description}</td>
                                     <td>{todo.done.toString()}</td>
-                                    <td>{todo.targetDate.toDateString()}</td>
+                                    <td>{todo.targetDate.toString()}</td>
                                 </tr>
                             )
                         )
