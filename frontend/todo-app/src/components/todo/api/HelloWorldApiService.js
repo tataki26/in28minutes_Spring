d19d0af276b167375058077e98fb6e562cd8531f
tataki26 <tataki26@gmail.com>
2023-09-25 23:09:27 +0900

    Spring Boot REST API 호출을 위해 React에 인증 헤더 추가하기
    - 보호된 API에 접근하기 위해서는 모든 요청 호출에 기본 인증 헤더를 전송해야 한다
    
    - 요청에 header 객체를 정의하고 Authorization 속성을 추가한다
    - preflight 요청에 대한 응답이 액세스 제어 체크를 통과하지 못했다는 에러가 뜬다
    
    * preflight
    - CORS의 일부로 보안을 유지하기 위한 요청 절차에서 사용된다
    - 실제 요청을 보내기 전에 서버에게 특별한 HTTP OPTIONS 요청을 먼저 보내고 서버의 응답을 확인하여 요청이 안전하게 수행될 수 있는지 확인한다
    - 이 OPTIONS 요청에 대한 액세스를 허용해줘야 한다

--- frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
+++ frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
@@ -14,4 +14,9 @@ export const retrieveHelloWorldBean
     = () => apiClient.get('/hello-world-bean');
 
 export const retrieveHelloWorldPathVariable
-    = (username) => apiClient.get(`/hello-world/path-variable/${username}`);
+    = (username) => apiClient.get(`/hello-world/path-variable/${username}`, {
+        headers: {
+            // Response to preflight request doesn't pass access control check => Authorization header
+            Authorization: 'Basic aW4yOG1pbnV0ZXM6ZHVtbXk='
+        }
+    });
