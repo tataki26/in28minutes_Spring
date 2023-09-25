8db40323bab029ec5c972d1468b9bf4b7a53751f
tataki26 <tataki26@gmail.com>
2023-09-26 00:56:12 +0900

    AuthContext에 기본 인증 토큰 설정하기
    - HelloWorldApi에도 공유 token 적용하기
    - context에 저장된 token을 가져와서 사용하기
    - api의 파라미터에 토큰 추가

--- frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
+++ frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
@@ -14,10 +14,10 @@ export const retrieveHelloWorldBean
     = () => apiClient.get('/hello-world-bean');
 
 export const retrieveHelloWorldPathVariable
-    = (username) => apiClient.get(`/hello-world/path-variable/${username}`, {
+    = (username, token) => apiClient.get(`/hello-world/path-variable/${username}`, {
         headers: {
             // Response to preflight request doesn't pass access control check => Authorization header
-            Authorization: 'Basic aW4yOG1pbnV0ZXM6ZHVtbXk='
+            Authorization: token
         }
     });
 
