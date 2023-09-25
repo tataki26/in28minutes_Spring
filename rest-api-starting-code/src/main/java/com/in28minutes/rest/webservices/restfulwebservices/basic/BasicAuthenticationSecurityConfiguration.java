629c2da0c34a542fe1610d82f9817bde4e58cd40
tataki26 <tataki26@gmail.com>
2023-09-25 22:49:48 +0900

    Spring Security로 Spring Boot REST API 보호하기
    - 인증을 하지 않고 REST API 요청을 보내면 401(인증되지 않음) 에러가 뜬다
    - 인증 헤더에 username과 password를 포함하여 전송할 수 있다
    
    - Spring Security를 클래스 경로에 추가하자마자 Spring Boot는 REST API에 있는 모든 리소스를 보호하기 위해 자동으로 필터를 설정한다
    - 따라서, 필터에 대한 커스터마이징이 필요하다
    
    - @Configuration 클래스에서 SecurityFilterChain을 정의한다
    - authorizeHttpRequest로 모든 요청이 인증을 요구하도록 설정한다
    - httpBasic으로 기본 인증(pop-up)을 설정한다
    - 상태가 없는 REST API로 세션을 관리하기 위해 sessionManagement를 설정한다
    - 이는 CSRF 비활성화를 위한 기본 전제이다
    - csrf.disable()을 통해 CSRF를 비활성화 시킨다
    - 요약: 모든 요청에 대해 인증을 활성화해서 기본 인증을 활설화하고 상태가 없는 세션을 설정해서 CSRF를 비활성화 시킨다
    
    - 필터 체인은 일반적으로 체이닝 방식으로 코드를 작성한다

---
+++ rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.in28minutes.rest.webservices.restfulwebservices.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+    // Filter chain
+    // disable csrf
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /*
+        // authenticate all requests
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+                );
+        // basic authentication ==> pop-up
+        http.httpBasic(Customizer.withDefaults());
+        // stateless session - rest api
+        http.sessionManagement(
+                session -> session.sessionCreationPolicy
+                        (SessionCreationPolicy.STATELESS));
+        http.csrf().disable();
+        return http.build();
+        */
+
+        // chaining
+        return http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+                )
+            .httpBasic(Customizer.withDefaults())
+            .sessionManagement(
+                session -> session.sessionCreationPolicy
+                        (SessionCreationPolicy.STATELESS))
+            .csrf().disable()
+            .build();
+    }
+}
