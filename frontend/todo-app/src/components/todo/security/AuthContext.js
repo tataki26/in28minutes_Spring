bfe002b5994378745a62fbb91820fd6c13957048
tataki26 <tataki26@gmail.com>
2023-09-26 00:51:03 +0900

    async와 await을 사용하여 기본 인증 API 호출하기
    - executeBasicAuthenticationService 함수는 응답에 대한 결과를 기다려야 한다
    - 따라서, 기존의 promise가 아닌 async await으로 동기 방식의 실행이 필요하다
    - 응답을 기다렸다가 성공(200)하면 인증을 승인하고 token을 컨텍스트에 저장한다
    - 실패 시에는 인증을 승인하지 않고 토큰을 null로 정의한다
    - try catch문으로 executeBasicAuthenticationService 자체가 실패했을 때도 에러 로직이 수행되도록 설정한다
    
    - ** 내부에서 async await을 정의하면 그 함수를 호출하는 함수에도 async await을 설정해야 한다 **
    
    - 반복되는 에러 로직을 logout으로 대체한다
    
    - token은 컨텍스트에 공유 state로 정의한다

--- frontend/todo-app/src/components/todo/security/AuthContext.js
+++ frontend/todo-app/src/components/todo/security/AuthContext.js
@@ -10,6 +10,7 @@ export default function AuthProvider({ children }) {
     // Put some state in the context
     const [isAuthenticated, setAuthenticated] = useState(false);
     const [username, setUsername] = useState(null);
+    const [token, setToken] = useState(null);
 
     // JavaScript object
     // const valueToBeShared = {number, isAuthenticated, setAuthenticated}
@@ -29,35 +30,38 @@ export default function AuthProvider({ children }) {
     }
     */
 
-    function login(username, password) {
+    async function login(username, password) {
         // Base64 encoding
         const baToken = 'Basic ' + window.btoa(username + ":" + password);
         
-        executeBasicAuthenticationService(baToken)
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
-        
-        setAuthenticated(false);
+        try {
+            // wait until this code is finished
+            const response = await executeBasicAuthenticationService(baToken)
 
-        /*
-        if(username === 'in28minutes' && password === 'dummy') {
-            setAuthenticated(true);
-            setUsername(username);
-            return true;
-        } else {
-            setAuthenticated(false);
-            setUsername(null);
+            if(response.status == 200) {
+                setAuthenticated(true);
+                setUsername(username);
+                // set into context
+                setToken(baToken);
+                return true;
+            } else {
+                logout();
+                return false;
+            }
+        } catch (error) {
+            logout();
             return false;
         }
-        */
     }
 
     function logout() {
         setAuthenticated(false);
+        setUsername(null);
+        setToken(null);
     }
 
     return (
-        <AuthContext.Provider value={ {isAuthenticated, username, login, logout} }>
+        <AuthContext.Provider value={ {isAuthenticated, username, token, login, logout} }>
             {children}
         </AuthContext.Provider>
     )
