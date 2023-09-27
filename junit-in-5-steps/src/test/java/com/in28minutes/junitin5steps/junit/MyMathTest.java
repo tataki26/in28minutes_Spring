c77e09a10d3e2d60bb3d418a8ee6f3eb002b9910
tataki26 <tataki26@gmail.com>
2023-09-27 11:02:17 +0900

    첫 코드에서 첫 단위 테스트 수행하기
    - 공통으로 사용하는 인스턴스는 메서드 밖에서 정의하여 공유
    - assertEquals 함수에서 기대값을 하드코딩하고 로직을 인라인으로 정의하여 테스트 코드를 간결하게 작성 가능
    
    - 단위 테스트 수행 시, 지속적 통합 방식을 사용하면 테스트를 통과하지 못할 경우 개발자가 즉시 메일을 받아 수정할 수 있도록 시스템을 구축할 수 있다

--- junit-in-5-steps/src/test/java/com/in28minutes/junitin5steps/junit/MyMathTest.java
+++ junit-in-5-steps/src/test/java/com/in28minutes/junitin5steps/junit/MyMathTest.java
@@ -5,19 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyMathTest {
+    private MyMath math = new MyMath();
+
     @Test
-    void test() {
-        // [강제] 테스트 통과X
-        // fail("Not yet implemented");
+    void calculateSum_ThreeMemberArray() {
+        assertEquals(6, math.calculateSum(new int[] {1,2,3}));
+    }
 
-        int[] numbers = {1,2,3};
-        MyMath math = new MyMath();
-        int result = math.calculateSum(numbers);
-        System.out.println(result);
-        int expectedResult = 5;
-        assertEquals(expectedResult, result);
-
-        // Absence of failure is success
-        // Test Condition or Assert
+    @Test
+    void calculateSum_ZeroLengthArray() {
+        assertEquals(0, math.calculateSum(new int[] {}));
     }
 }
