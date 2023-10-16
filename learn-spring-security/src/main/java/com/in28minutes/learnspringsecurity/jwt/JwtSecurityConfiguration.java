5a7013546727826c52877850ad0dc21e199c63fe
tataki26 <tataki26@gmail.com>
2023-10-16 23:45:38 +0900

    Spring Security와 Spring Boot로 JWT 인증 설정하기 2
    - JWT 디코딩 설정을 위한 RSA 암호화 키 쌍 생성
    
    * RSA
    - 공개 키 및 개인 키가 필요한 암호화 및 서명 알고리즘
    
    * keyPair()
    - KeyPairGenerator를 사용하여 RSA 암호화 키 생성
    - 2048 비트로 초기화된 RSA 키 쌍 생성 및 반환
    
    * rsaKey(KeyPair keyPair)
    - keyPair 메서드에서 생성한 키 쌍을 사용하여 RSAKey 객체를 생성
    - RSAKey 객체에는 공개 키, 개인 키, 그리고 고유한 키 ID가 설정 된다
    - JWT의 서명 검증 과정에서 공개 키는 JWT의 서명을 검증하는 데 사용되며, 개인 키는 JWT의 생성에 사용된다
    
    * jwkSource(RSAKey rsaKey)
    - RSAKey 객체를 사용하여 JWK (JSON Web Key) 집합인 JWKSet을 생성
    - JWKSet은 JWKSelector와 함께 사용된다
    - JWT 디코더가 JWT 서명 검증을 수행할 때 필요한 공개 키 정보 제공(JKW 정보 캡슐화)
    - JWKSelector는 특정 키를 선택하도록 도와주는 역할
    
    * jwtDecoder(RSAKey rsaKey)
    - RSAKey 객체를 사용하여 JWT 디코딩을 위한 JwtDecoder 생성
    - JwtDecoder는 RSA 공개 키를 기반으로 JWT 디코딩하고 JWT의 서명 검증(유효성, 안전성)
    
    * 요약
    - JWT는 웹 애플리케이션에서 사용자 인증 및 권한 부여와 같은 보안 기능을 구현하는 데 사용된다
    - RSA 키 쌍은 이러한 JWT의 보안을 강화하는 데 중요한 역할을 한다
    
    - ** openssl 툴을 통해 RSA 공개 키와 비밀 키를 만들 수 있다 **

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtSecurityConfiguration.java
@@ -1,6 +1,14 @@
 package com.in28minutes.learnspringsecurity.jwt;
 
 import com.in28minutes.learnspringsecurity.basic.UserRoles;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -12,10 +20,18 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+import java.util.UUID;
 
 @Configuration
 public class JwtSecurityConfiguration {
@@ -75,5 +91,44 @@ public class JwtSecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+    }
+
+    // create key pair
+    @Bean
+    public KeyPair keyPair() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // create RSAKey object with KeyPair
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) {
+        return new RSAKey
+                // set public key
+                .Builder((RSAPublicKey) keyPair.getPublic())
+                // set private key
+                .privateKey(keyPair.getPrivate())
+                // set key id
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    // create JWKSource with RSAKey
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, context) -> jwkSelector.select(jwkSet);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder
+                .withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
     }
 }
