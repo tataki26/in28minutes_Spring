cd51abe297534ba817f5e1969e6eace906f412f8
tataki26 <tataki26@gmail.com>
2023-10-16 16:05:31 +0900

    Spring Security 살펴 보기 - JDBC를 통해 사용자 자격 증명 저장하기
    - 데이터베이스에 연결하기 위해 JDBC 사용
    
    - Spring Security에서는 기본적으로 프레임을 사용 해제 하므로 h2 콘솔과 같이 HTML 프레임을 사용하면 필터 체인에 사용 설정을 추가 해야 한다
    - http.headers().frameOptions().sameOrigin(): 요청이 동일한 오리진에서 오는 경우 해당 애플리케이션에 프레임을 허용하는 옵션
    
    - spring security core에는 사용자 스키마 데이터 정의 언어(DDL)이 이미 정의되어 있다
    - USER: 사용자 이름 / 패스워드
    - Authorities: 사용자 이름 / 권한
    
    - EmbeddedDatabaseBuilder로 새로운 임베디드 데이터 베이스(h2)를 생성하고 빈으로 등록한다
    - 스크립트로는 기존에 정의된 DEFAULT_USER_SCHEMA_DDL을 사용한다
    - 프로그램 실행 시 해당 스크립트가 실행된다
    
    - DataSource을 입력 받는 UserDetailsService 메서드를 Bean으로 추가하여 default 사용자를 추가한다
    - JdbcUserDetailsManager에 사용자를 등록하고 해당 객체를 반환한다
    - 위 객체는 JDBC 사용자 관리 서비스로 사용자와 그룹 모두에 CRUD 연산을 제공한다
    
    - 하나의 사용자에 여러 권한을 부여할 수 있다

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,13 +2,18 @@ package com.in28minutes.learnspringsecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -30,9 +35,12 @@ public class BasicAuthSecurityConfiguration {
         http.httpBasic();
         // disable csrf
         http.csrf().disable();
+        // same origin request -> enable frame option
+        http.headers().frameOptions().sameOrigin();
         return http.build();
     }
 
+    /*
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("in28minutes")
@@ -46,5 +54,33 @@ public class BasicAuthSecurityConfiguration {
                 .build();
 
         return new InMemoryUserDetailsManager(user, admin);
+    }
+    */
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        UserDetails user = User.withUsername("in28minutes")
+                .password("{noop}dummy")
+                .roles(UserRoles.USER.name())
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}dummy")
+                .roles(UserRoles.ADMIN.name(), UserRoles.USER.name())
+                .build();
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
     }
 }
