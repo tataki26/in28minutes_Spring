bc6015427e083f4e2d6a3dd1c179e9fc8daebedf
tataki26 <tataki26@gmail.com>
2023-10-16 15:31:31 +0900

    Spring Security 살펴 보기 - 메모리에 사용자 자격 증명 저장하기
    - 테스트 목적으로 사용자 자격 증명을 메모리에 저장할 수 있다(프로덕션 환경에서는 권장x)
    - 그 외에 데이터베이스 저장, LDAP(경량 디렉터리 액세스 프로토콜) 사용 등의 방법이 있다
    
    - application.properties에 사용자 정보를 설정하는 것은 인메모리 방식이다
    - UserDetailsService 객체를 Bean으로 등록하여 여러 사용자 정보를 생성할 수 있다(사용자 별 데이터를 로드하는 코어 인터페이스)
    - 유저 이름과 비밀 번호, 역할 등을 설정할 수 있다
    - ** {noop}: Spring Security에서 사용자 비밀번호를 저장할 때 사용되는 Password Encoder(비밀번호 인코더)의 일종 **

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,5 +31,20 @@ public class BasicAuthSecurityConfiguration {
         // disable csrf
         http.csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("in28minutes")
+                .password("{noop}dummy")
+                .roles(UserRoles.USER.name())
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}dummy")
+                .roles(UserRoles.ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
---
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/UserRoles.java
@@ -0,0 +1,5 @@
+package com.in28minutes.learnspringsecurity.basic;
+
+public enum UserRoles {
+    USER, ADMIN
+}
